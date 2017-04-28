2a8f0653a369964c2c41ebc829b7627857ef3db1
Daniel McDonald <mcdonadt@colorado.edu>
2017-04-28 16:58:24 -0700

    Had the wrong category in the query...

--- test.sh
+++ test.sh
@@ -152,5 +152,5 @@ fi
 
 echo "10317.000033804" > exp_metadata_full.txt
 echo "10317.000001378" >> exp_metadata_full.txt
-redbiom search metadata-full "antibiotics where age < 25" > obs_metadata_full.txt
+redbiom search metadata-full "antibiotics where AGE_YEARS < 25" > obs_metadata_full.txt
 md5test obs_metadata_full.txt exp_metadata_full.txt
