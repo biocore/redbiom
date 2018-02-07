b874d35f0f2f4688924b682ded446e0aaee43d80
Daniel McDonald <mcdonadt@colorado.edu>
2018-02-07 15:47:31 -0800

    The style gods were unhappy

--- redbiom/util.py
+++ redbiom/util.py
@@ -126,7 +126,7 @@ def float_or_nan(t):
     import numpy as np
     try:
         return float(t)
-    except Exception as e:
+    except Exception:
         return np.nan
 
 
@@ -352,5 +352,5 @@ def stems(stops, stemmer, string):
 
         try:
             yield stemmer.stem(word).lower()
-        except Exception as e:
+        except Exception:
             continue
